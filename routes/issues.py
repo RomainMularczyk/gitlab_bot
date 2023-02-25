d07173bca65e7a17b849e93110283f10bd6e8464
Debian <debian@vps-a39ece5d.vps.ovh.net>
2023-02-25 01:39:05 +0000

    [MOD] new_issue route

--- routes/issues.py
+++ routes/issues.py
@@ -1,9 +1,10 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 from bot.main import client, main
 
 router = APIRouter()
 
 
-@router.get("/new_issue")
-async def new_issue():
+@router.post("/new_issue")
+async def new_issue(request: Request):
+    print(await request.body())
     await client.get_cog("CogIssues").gitlab_trigger()
