f1f697306cd9702c096ed0df092fd5e3ba759533
Debian <debian@vps-a39ece5d.vps.ovh.net>
2023-02-27 08:30:18 +0000

    [ADD] member intents

--- bot/client/intents.py
+++ bot/client/intents.py
@@ -6,4 +6,7 @@ def load_intents() -> Intents:
     Return Discord intents.
     """
     intents = Intents.default()
+    # Authorize bot to see users
+    intents.members = True
+    intents.presences = True
     return intents
