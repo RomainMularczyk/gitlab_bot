22490351cc9ceb16c0c0a442d5b051e5d35a15a6
Debian <debian@vps-a39ece5d.vps.ovh.net>
2023-02-27 08:30:05 +0000

    [DEL] dotenv

--- bot/client/client.py
+++ bot/client/client.py
@@ -1,12 +1,6 @@
 import os
-from dotenv import load_dotenv
 from discord import Intents
 from discord.ext.commands import Bot
-
-# ---- Load dotenv ----
-load_dotenv()
-token: str = os.environ.get("DISCORD_BOT_TOKEN")
-print(token)
 
 
 class DiscordClient(Bot):
