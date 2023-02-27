4d79704f14647d52bcb84b0626734fd5c809a27f
Debian <debian@vps-a39ece5d.vps.ovh.net>
2023-02-27 08:30:49 +0000

    [ADD] cog issue

--- bot/cogs/cog_issues.py
+++ bot/cogs/cog_issues.py
@@ -1,21 +1,48 @@
-from discord.ext.commands import Bot, Cog, Context
+import os
+from typing import Dict
+from discord.ext.commands import Bot, Cog
+from dotenv import load_dotenv
+from bot.formaters.issue_formater import format_issue
+from models.Assignee import Assignee
+from bot.members.retrieve_member import retrieve_member
+from errors.Discord import NoSuchDiscordUser
+
+# ---- Load dotenv ----
+load_dotenv(".env.discord")
 
 
-class CogIssues(Cog):
+class CogIssues(Cog, name="CogIssues"):
     def __init__(self, bot: Bot):
         self.bot = bot
+        self.channel_id = int(os.environ.get("DISCORD_MISSIONS_CHANNEL_ID"))
 
     @Cog.listener()
-    async def on_ready(self):
+    async def on_ready(self) -> None:
+        """
+        Log 'ready' when loaded.
+        """
         print("CogsIssues ready.")
 
-    async def gitlab_trigger(self):
+    async def gitlab_trigger(self, assignee: Assignee, issue: Dict) -> None:
         """
         Listen to GitLab webhook trigger to send tasks to a user.
+
+        Parameters
+        ----------
+        issue : Dict
+            A currated GitLab issue.
         """
-        pass
-        # await self.bot.get_channel(id)
+        try:
+            member_id = retrieve_member(self.bot.get_all_members(), assignee)
+        except NoSuchDiscordUser:
+            raise NoSuchDiscordUser(assignee.username)
+
+        formated_issue = format_issue(member_id, issue)
+        await self.bot.get_channel(self.channel_id).send(formated_issue)
 
 
 async def setup(bot: Bot) -> None:
+    """
+    Setup function for automated cogs discovery.
+    """
     await bot.add_cog(CogIssues(bot))
