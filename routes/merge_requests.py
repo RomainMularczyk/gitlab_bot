fd9f837be7f21a82c45dc08f76b1076ac4ff3db5
Debian <debian@vps-a39ece5d.vps.ovh.net>
2023-03-25 17:35:31 +0000

    [ADD] merge request controller

--- routes/merge_requests.py
+++ routes/merge_requests.py
@@ -1,10 +1,31 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+from bot.main import client
+from models.MergeRequest import MergeRequest
+from services.MergeRequests import MergeRequests
+from errors.Discord import NoSuchDiscordUser
+from errors.GitLab import GitLabAttributeNotFound
 
 
 router = APIRouter()
 
 
 @router.post("/new_merge_request", status_code=200)
-async def new_merge_request(merge_request):
-    print("okay yeah")
-    print(merge_request)
+async def new_merge_request(merge_request: MergeRequest):
+    try:
+        currated_merge_request = MergeRequests.currate_merge_request(
+            merge_request
+        )
+    except GitLabAttributeNotFound as e:
+        raise HTTPException(
+            status_code=422, detail="GitLab attribute not provided."
+        )
+
+    try:
+        await client.get_cog("CogMergeRequest").gitlab_trigger(
+            merge_request.assignees, currated_merge_request
+        )
+    except NoSuchDiscordUser as e:
+        raise HTTPException(
+            status_code=404,
+            detail="Discord user not found. Username : {e.username}.",
+        )
